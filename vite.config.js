a8e7dbad534940ffd5f040e22256101ea860d673
Sainath Atmaram Gaikwad <89898283+SainathGaikwad@users.noreply.github.com>
2024-03-19 14:08:21 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.glb"],
-  base: "/Sainath-Gaikwad-Portfolio/",
+  // base: "/Sainath-Gaikwad-Portfolio/",
 });
